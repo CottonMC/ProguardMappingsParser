2ccb9c7a034c9af0dc3fae40a35cc52ed8062e53
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-02-15 17:37:22 +0200

    Output the line and class data for methods in Writer.toProguardMappings()

--- src/main/kotlin/io/github/cottonmc/proguardparser/Writer.kt
+++ src/main/kotlin/io/github/cottonmc/proguardparser/Writer.kt
@@ -4,9 +4,29 @@ fun ProjectMapping.toProguardMappings(): List<String> =
     classes.flatMap { c ->
         val fields = c.fields.map { "${it.type} ${it.from} -> ${it.to}" }
         val methods = c.methods.map {
-            // TODO: Add back line numbers
+            val newLineNums =
+                if (it.startLine != MethodMapping.MISSING_LINE)
+                    "${it.startLine}:${it.endLine}:"
+                else
+                    ""
+
+            val oldLineNums: String =
+                if (it.originalStartLine != MethodMapping.MISSING_LINE) {
+                    if (it.originalEndLine != MethodMapping.MISSING_LINE) {
+                        ":${it.originalStartLine}:${it.originalEndLine}"
+                    } else {
+                        ":${it.originalStartLine}"
+                    }
+                } else {
+                    ""
+                }
+
+            val originalClass =
+                if (it.originalClass != null) it.originalClass + '.'
+                else ""
+
             val params = it.parameters.joinToString(separator = ",")
-            "${it.returnType} ${it.from}($params) -> ${it.to}"
+            "$newLineNums${it.returnType} $originalClass${it.from}($params)$oldLineNums -> ${it.to}"
         }
         listOf("${c.from} -> ${c.to}:") + (fields + methods).map { "    $it" }
     }
